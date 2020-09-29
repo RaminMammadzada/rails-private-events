f8123f5f107d4f1525fb33ce633398f7acffc90f
jurgen1c <jurgen1c@gmail.com>
2020-09-29 15:00:01 -0600

    Add event model,controller & views and add associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_172612) do
+ActiveRecord::Schema.define(version: 2020_09_29_204639) do
+
+  create_table "events", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "description"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
