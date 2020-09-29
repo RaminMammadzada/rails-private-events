9e16a6612d19e32ff490e0d487bcf78effc832fb
Ramin Mammadzada <remki-design@Hotmail.com>
2020-09-29 20:50:08 +0300

    Create session controller and views.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "You are successfully logged in!"
+    else
+      flash.now[:alert] = "You email is not valid!"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "You are logged out!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
