f03074e4374b9d80ee884c02f384506ea5a34893
Ramin Mammadzada <remki-design@Hotmail.com>
2020-09-29 21:17:07 +0300

    Add Session controller, helper mehtod to get current user, update main page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+
+  root "users#index"
+
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
+
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
