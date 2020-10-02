ce4c9e30b50314af9195347e8712100968be6206
jurgen1c <jurgen1c@gmail.com>
2020-10-02 14:40:35 -0600

    Add creator id column, fix new event error & move logic out of view

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,36 @@
 module EventsHelper
+  def display_errors(event)
+    if event.errors.any?
+      pluralize(event.errors.count, 'error')
+      messages = []
+      event.errors.full_messages.each do |msg|
+        messages.push(msg)
+      end
+
+      error_counts_message = content_tag(:h2, event.errors.count.to_s + ' errors prohibited this post from being saved:',
+                                         class: 'total_errors')
+      error_message = content_tag(:ul, class: 'all_errors') do
+        messages.each { |msg| concat content_tag(:li, msg) }
+      end
+
+      all_error_contents = content_tag(:div, error_counts_message + error_message, class: 'all_error_contents')
+
+      return all_error_contents
+    end
+
+    nil
+  end
+
+  def display_join(current_user, event)
+    if current_user.attended_events.include? event
+      content_tag(:td, (link_to 'Unjoin', destroy_path(event_id: event.id, attendee_id: current_user.id)))
+    else
+      content_tag(:td, (link_to 'Join', join_path(event_id: event.id)))
+    end
+  end
+  def creator_destroy(current_user, event)
+    if current_user == event.creator
+      content_tag(:td, (link_to 'Destroy', event, method: :delete, data: { confirm: 'Are you sure?' }))
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_212425) do
+ActiveRecord::Schema.define(version: 2020_10_02_195250) do
 
   create_table "attendees", force: :cascade do |t|
     t.integer "user_id"
